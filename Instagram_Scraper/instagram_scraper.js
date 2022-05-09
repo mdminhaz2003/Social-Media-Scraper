534dff1720f03c6d9d8ea83f13a71fa521de0c7d
Md. Minhaz <mdm047767@gmail.com>
2022-05-09 12:23:12 +0600

    update javascript code

--- Instagram_Scraper/instagram_scraper.js
+++ Instagram_Scraper/instagram_scraper.js
@@ -4,39 +4,10 @@ const fs = require('fs');                                       // file system m
 const Insta = require('scraper-instagram');                     // Instagram scraper module
 const InstaClient = new Insta();                                // Instagram module instance
 
-let csvFileText = "Name,Website,Instagram_Username,Instagram_Followers,Instagram_Following,Instagram_Total_Posts\n"
-
-/**
- * Instagram Scraper Function for scrap data from instagram
- * @param {String} username
- * @param {Number} indexNumber
- */
- async function instagramScraper (username, indexNumber) {
-    let identifier = result[indexNumber];
-    await InstaClient.getProfile(username)
-        .then(profile => {
-            let followersCount = profile.followers;
-            let followingCount = profile.following;
-            let totalPostsCount = profile.posts;
-            identifier.Instagram_Followers_Count = followersCount;
-            identifier.Instagram_Following_Count = followingCount;
-            identifier.Instagram_Total_Posts_Count = totalPostsCount;
-        })
-        .catch(error => {
-            if (error) {
-                identifier.Instagram_Followers_Count = "Not Found";
-                identifier.Instagram_Following_Count = "Not Found";
-                identifier.Instagram_Total_Posts_Count = "Not Found";
-            }
-        })
-        .finally(() => {
-            let newData = `${identifier.Name},${identifier.Web},${identifier.Instagram_User},${identifier.Instagram_Followers_Count},${identifier.Instagram_Following_Count},${identifier.Instagram_Total_Posts_Count}\n`;
-            csvFileText = csvFileText.concat(newData);
-            console.log(`${indexNumber} is passed...`);
-        })
-}
-
-
+let csvFileHeaders = "Name,Website,Instagram_Username,Instagram_Followers,Instagram_Following,Instagram_Total_Posts\n"
+fs.appendFile('./instagram_output_data.csv', csvFileHeaders, (error) => {
+    console.log(error);
+});
 const result = [];
 
 fs.createReadStream(process.env.CSV_FILE_PATH)
@@ -46,27 +17,43 @@ fs.createReadStream(process.env.CSV_FILE_PATH)
     })
     .on("end", () => {
         InstaClient.authBySessionId(process.env.INSTAGRAM_SESSION_ID)
-            .then((account) => {
-                if (account) {
-                    for (let item = 0; item < result.length; item++) {
-                        let instagramUserName = result[item].Instagram_User;
-                        instagramScraper(instagramUserName, item)
-                            .finally(() => {
-                                if (item == result.length - 1) {
-                                    fs.writeFile("./instagram_output_data.csv", csvFileText, error => {
-                                        if (error) {
-                                            console.log(error);
-                                        } else {
-                                            console.log("File Written successful.... :)");
-                                        }
-                                    });
-                                }
+            .then(() => {
+                for (let item = 0; item < result.length; item++) {
+                    let instagramUserName = result[item].Instagram_User;
+                    let identifier = result[item];
+                    if (instagramUserName != "") {
+                        InstaClient.getProfile(instagramUserName)
+                        .then((profile) => {
+                            let followersCount = profile.followers;
+                            let followingCount = profile.following;
+                            let totalPostsCount = profile.posts;
+                            identifier.Instagram_Followers_Count = followersCount;
+                            identifier.Instagram_Following_Count = followingCount;
+                            identifier.Instagram_Total_Posts_Count = totalPostsCount;
+                        })
+                        .catch((error) => {
+                            if (error) {
+                                identifier.Instagram_Followers_Count = "Not Found";
+                                identifier.Instagram_Following_Count = "Not Found";
+                                identifier.Instagram_Total_Posts_Count = "Not Found";
+                            }
+                        })
+                        .finally(() => {
+                            let newData = `${identifier.Name},${identifier.Web},${identifier.Instagram_User},${identifier.Instagram_Followers_Count},${identifier.Instagram_Following_Count},${identifier.Instagram_Total_Posts_Count}\n`;
+                            console.log(newData);
+                            fs.appendFile('./instagram_output_data.csv', newData, (err) => {
+                                if (err) throw err;
+                                console.log('New Data was appended to file!');
                             });
+                        });
+                    } else {
+                        console.log("Empty Cell !!!");
+                        continue;
                     }
+
                 }
             })
-            .catch(error => {
+            .catch((error) => {
                 console.log(error);
             });
     });
-
