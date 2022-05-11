8b97d58f5930642dd9d909d15e77f8059bf52acf
Md. Minhaz <mdm047767@gmail.com>
2022-05-12 00:14:01 +0600

    Twitter Scraper Completet.

---
+++ Twitter_Scraper/README.md
@@ -0,0 +1,51 @@
+
+# **Twitter Scraper (Total Tweets, Followers and Followings)**
+
+**This program will help you to properly scrap total tweets, total followers and total following count form Twitter**
+
+
+## :link: &nbsp;Installation
+
+Install Twitter Scraper with python (make sure python and pip already installed in your computer)
+
+First, Open your terminal in current path and then copy those code and paste in terminal :-
+```bash
+    pip install -r requirements.txt
+```
+If you are using python3 version then you can try it :-
+```bash
+    pip3 install -r requirements.txt
+```
+For run this project you can simply write in terminal like this :
+```bash
+    python twitter_scraper.py
+```
+Also, If you are using python3 version then you can try it :-
+```bash
+    python3 twitter_scraper.py
+```
+If you want to create executable file for your operating system :-
+```bash
+    pyinstaller --onefile pinterest_scraper.py
+```
+
+## :link: &nbsp;Download Webdriver
+If you are using Chrome 101 then download webdriver for Linux, Windows and Mac from this link :-
+```bash
+    https://chromedriver.storage.googleapis.com/index.html?path=101.0.4951.41/
+```
+Else if you are using Chrome 102 version then goto this link :-
+```bash
+    https://chromedriver.storage.googleapis.com/index.html?path=102.0.5005.27/
+```
+### :link: &nbsp;Connect with Author
+
+<p align="center">
+<a href="https://www.buymeacoffee.com/mdminhaz2003"><img src="https://img.shields.io/badge/-Buy me a coffee-000000?style=for-the-badge&logo=buymeacoffee&logoColor=yellow"/></a>
+<a href="https://www.youtube.com/easycoding2021/"><img src="https://img.shields.io/badge/-Easy Coding-FF0000?style=for-the-badge&logo=YouTube&logoColor=white"/></a>
+<a href="https://www.facebook.com/mdminhaz2003/"><img src="https://img.shields.io/badge/-Md. Minhaz-3423A6?style=for-the-badge&logo=Facebook&logoColor=white"/></a>
+<a href="https://www.linkedin.com/in/mdminhaz2003/"><img src="https://img.shields.io/badge/-Md. Minhaz-0077B5?style=for-the-badge&logo=Linkedin&logoColor=white"/></a>
+<a href="mailto:mdm047767@gmail.com"><img src="https://img.shields.io/badge/-Mail-D14836?style=for-the-badge&logo=Gmail&logoColor=white"/></a>
+<a href="https://instagram.com/mdminhaz2003/"><img src="https://img.shields.io/badge/-Md. Minhaz-E4405F?style=for-the-badge&logo=Instagram&logoColor=white"/></a>
+<a href="https://twitter.com/easycoding2021/"><img src="https://img.shields.io/badge/-Easy Coding-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white"/></a>
+</p>
---
+++ Twitter_Scraper/twitter_scraper.py
@@ -0,0 +1,88 @@
+import os
+import numpy as np
+import pandas as pd
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+load_dotenv(dotenv_path="./.env")
+
+df = pd.read_csv(filepath_or_buffer=os.getenv("CSV_FILE_PATH"))
+
+twitter_username = df['Twitter_User'].dropna()
+
+options = Options()
+driver = webdriver.Chrome(executable_path=os.getenv("DRIVER_PATH"), options=options)
+driver.get("https://www.twitter.com")
+driver.implicitly_wait(time_to_wait=5)
+driver.execute_script("window.open('');")
+
+
+def scraper(index: int) -> dict:
+    driver.switch_to.window(driver.window_handles[index])
+    driver.get(f'https://www.twitter.com/{username}')
+    driver.implicitly_wait(5)
+    data = {}
+    try:
+        total_tweets_count = driver.find_element(
+            by=By.XPATH,
+            value='/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/div[1]/div/div/div/div/div[2]/div/div'
+        )
+        data["tweets_count"] = total_tweets_count.text
+    except Exception as e:
+        print(e)
+        data["tweets_count"] = "Not Found"
+
+    try:
+        total_followings_count = driver.find_element(
+            by=By.CSS_SELECTOR,
+            value='a[href$="/following"]'
+        )
+        data["following_count"] = total_followings_count.text
+    except Exception as e:
+        print(e)
+        data["following_count"] = "Not Found"
+
+    try:
+        total_followers_count = driver.find_element(
+            by=By.CSS_SELECTOR,
+            value='a[href$="/followers"]'
+        )
+        data["followers_count"] = total_followers_count.text
+    except Exception as e:
+        print(e)
+        data["followers_count"] = "Not Found"
+    return data
+
+
+all_data = np.array(["Name", "Web", "Twitter Username", "Total Tweets", "Total Followers", "Total Following"], ndmin=2)
+index_number = 1
+for username in twitter_username:
+    if username != "":
+        df_index_number = df[df['Twitter_User'] == username].index[0]
+        single_data = np.empty(shape=[1, 6], dtype='<U100')
+        single_data[0, 0] = df['Name'][df_index_number]
+        single_data[0, 1] = df['Web'][df_index_number]
+        single_data[0, 2] = df['Twitter_User'][df_index_number]
+
+        if index_number % 2 == 1:
+            single_data_set = scraper(index=0)
+            single_data[0, 3] = single_data_set["tweets_count"]
+            single_data[0, 4] = single_data_set["followers_count"]
+            single_data[0, 5] = single_data_set["following_count"]
+        elif index_number % 2 == 0:
+            single_data_set = scraper(index=1)
+            single_data[0, 3] = single_data_set["tweets_count"]
+            single_data[0, 4] = single_data_set["followers_count"]
+            single_data[0, 5] = single_data_set["following_count"]
+        else:
+            pass
+        all_data = np.append(all_data, single_data, axis=0)
+    else:
+        pass
+    index_number += 1
+print(all_data)
+new_dataframe = pd.DataFrame(data=all_data[1:], columns=all_data[0])
+new_dataframe.to_csv("twitter_result.csv", index=False)
+driver.quit()
